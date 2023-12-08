8893aac2fdebd52ede8e81ca27eba3c6c1f30cb9
veeru <duppalapudiveerraju@gmail.com>
2023-12-08 16:54:55 +0530

    new commit

---
+++ demo/src/main/java/com/example/demo/controller/Endpoint.java
@@ -0,0 +1,19 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class Endpoint {
+
+	@GetMapping("/message")
+	public String getMessage() {
+		return "Welcome to Spring boot App";
+	}
+	public static void main(String[] args) {
+	
+		System.out.println();
+	}
+}
